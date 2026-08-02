c9478f65330e78da1c30f5222a22612e99efe9cd
drwnd <markus.vonhaenisch@gmail.com>
2026-08-02 20:06:25 +0200

    It's slower :(

--- src/main/java/game/server/materials_data/ByteArrayCompressor.java
+++ src/main/java/game/server/materials_data/ByteArrayCompressor.java
@@ -2,7 +2,8 @@ package game.server.materials_data;
 
 import core.utils.ByteArrayList;
 
-import java.nio.ByteBuffer;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 
 import static game.server.materials_data.MaterialsData.*;
 
@@ -13,24 +14,24 @@ final class ByteArrayCompressor {
     }
 
     static void compressMaterials(ByteArrayList data, byte[] uncompressedMaterials, int sizeBits) {
-        ByteBuffer byteBuffer = ByteBuffer.wrap(uncompressedMaterials);
-        compressMaterials(data, byteBuffer, sizeBits, 0, 0, 0, 0);
+        long[] longMaterials = MemorySegment.ofArray(uncompressedMaterials).toArray(ValueLayout.OfByte.JAVA_LONG_UNALIGNED);
+        compressMaterials(data, longMaterials, sizeBits, 0, 0, 0, 0);
     }
 
-    private static int compressMaterials(ByteArrayList data, ByteBuffer uncompressedMaterials, int sizeBits, int startIndex, int inChunkX, int inChunkY, int inChunkZ) {
+    private static int compressMaterials(ByteArrayList data, long[] uncompressedMaterials, int sizeBits, int startIndex, int inChunkX, int inChunkY, int inChunkZ) {
         if (isHomogenous(MaterialsData.getUncompressedIndex(inChunkX, inChunkY, inChunkZ), 1 << sizeBits * 3, uncompressedMaterials))
             return addHomogenous(data, uncompressedMaterials, inChunkX, inChunkY, inChunkZ);
         if (sizeBits <= 1) {
-            long materials = uncompressedMaterials.getLong(getUncompressedIndex(inChunkX, inChunkY, inChunkZ));
+            long materials = uncompressedMaterials[getUncompressedIndex(inChunkX, inChunkY, inChunkZ) >> 3];
             data.add((byte) (getTypes(materials) | DETAIL));
-            data.add((byte) (materials >> 56));
-            data.add((byte) (materials >> 40));
-            data.add((byte) (materials >> 48));
-            data.add((byte) (materials >> 32));
-            data.add((byte) (materials >> 24));
-            data.add((byte) (materials >> 8));
-            data.add((byte) (materials >> 16));
             data.add((byte) materials);
+            data.add((byte) (materials >> 16));
+            data.add((byte) (materials >> 8));
+            data.add((byte) (materials >> 24));
+            data.add((byte) (materials >> 32));
+            data.add((byte) (materials >> 48));
+            data.add((byte) (materials >> 40));
+            data.add((byte) (materials >> 56));
             return DETAIL_BYTE_SIZE;
         }
 
@@ -59,18 +60,18 @@ final class ByteArrayCompressor {
         return offset;
     }
 
-    private static boolean isHomogenous(int startIndex, int length, ByteBuffer uncompressedMaterials) {
-        long target = uncompressedMaterials.getLong(startIndex);
+    private static boolean isHomogenous(int startIndex, int length, long[] uncompressedMaterials) {
+        long target = uncompressedMaterials[startIndex >> 3];
         if (!isHomogenous(target)) return false;
-        int endIndex = startIndex + length;
-        for (int index = startIndex + 8; index < endIndex; index += 8)
-            if (uncompressedMaterials.getLong(index) != target) return false;
+        int endIndex = startIndex + length >> 3;
+        for (int index = startIndex + 8 >> 3; index < endIndex; index++)
+            if (uncompressedMaterials[index] != target) return false;
         return true;
     }
 
-    private static int addHomogenous(ByteArrayList data, ByteBuffer uncompressedMaterials, int inChunkX, int inChunkY, int inChunkZ) {
+    private static int addHomogenous(ByteArrayList data, long[] uncompressedMaterials, int inChunkX, int inChunkY, int inChunkZ) {
         int index = getUncompressedIndex(inChunkX, inChunkY, inChunkZ);
-        byte material = (byte) (uncompressedMaterials.getLong(index) >> (index & 7) * 8);
+        byte material = (byte) (uncompressedMaterials[index >> 3] >> (index & 7) * 8);
         data.add((byte) (getType(material) | HOMOGENOUS));
         data.add(material);
         return HOMOGENOUS_BYTE_SIZE;
